35df0cacb256b0832c7db529bb95daf644bf1eb5
Ilya Kolbeko <kolbeko@yahoo.com>
2022-03-18 03:27:12 +0300

    add road move feature

--- RacingGame/GameScene.swift
+++ RacingGame/GameScene.swift
@@ -10,79 +10,41 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    var player: SKSpriteNode!
+    var road1: SKSpriteNode!
+    var road2: SKSpriteNode!
+
     override func didMove(to view: SKView) {
+        // Set road
+        road1 = SKSpriteNode(imageNamed: "road")
+        road1.size = self.frame.size
+        road1.anchorPoint = CGPoint(x: 0, y: 0)
+        road1.position = CGPoint(x: 0, y: 0)
+        addChild(road1)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        road2 = SKSpriteNode(imageNamed: "road")
+        road2.size = self.frame.size
+        road2.anchorPoint = CGPoint(x: 0, y: 0)
+        road2.position = CGPoint(x: 0, y: road1.size.height)
+        addChild(road2)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        moveScene()
+    }
+    
+    func moveScene() {
+        //make road move
+        road1.position = CGPoint(x: road1.position.x, y: road1.position.y - 10)
+        road2.position = CGPoint(x: road2.position.x, y: road2.position.y - 10)
+        
+        //check road position
+        if road1.position.y < -road1.size.height {
+            road1.position = CGPoint(x: road1.position.x, y: road2.position.y + road2.size.height)
+        }
+        
+        if road2.position.y < -road2.size.height {
+            road2.position = CGPoint(x: road2.position.x, y: road1.position.y + road1.size.height)
+        }
     }
 }
--- RacingGame/GameViewController.swift
+++ RacingGame/GameViewController.swift
@@ -10,31 +10,24 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+            let scene = GameScene (size: view.bounds.size) // Создать экземпляр объекта класса GameScene с помощью кода
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -42,7 +35,7 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
